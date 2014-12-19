32ac0acafa1c493906701608c2802ea0c853cd02
Nikunj Gupta <nikunjboss@gmail.com>
2014-12-19 21:32:47 +0530

    Update 20141218134338_create_post_quests.rb

--- db/migrate/20141218134338_create_post_quests.rb
+++ db/migrate/20141218134338_create_post_quests.rb
@@ -1,7 +1,11 @@
 class CreatePostQuests < ActiveRecord::Migration
   def change
     create_table :post_quests do |t|
-
+    t.string :ques_text
+      t.string :t_id
+      t.string :first
+      t.string :upvote
+      t.string :follow
      t.timestamps
     end
   end
