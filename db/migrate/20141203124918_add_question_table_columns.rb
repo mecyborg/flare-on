c192e986b79ba5174f80f0175d4897ecf5091f3e
mecyborg <nikunjboss@gmail.com>
2014-12-19 22:45:24 +0530

    post-quest-fix1

--- db/migrate/20141203124918_add_question_table_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddQuestionTableColumns < ActiveRecord::Migration
-  def change
-    add_column :post_ques, :ques_text, :string
-    add_column :post_ques, :t_id, :integer
-    add_column :post_ques, :first, :string,:default => 'NO' 
-  end
-end
