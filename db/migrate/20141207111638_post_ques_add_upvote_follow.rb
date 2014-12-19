cd2c69327a221f78c6793dbef110461b75e6489c
mecyborg <nikunjboss@gmail.com>
2014-12-19 21:29:21 +0530

    post-quest-migration-fix

--- db/migrate/20141207111638_post_ques_add_upvote_follow.rb
+++
@@ -1,7 +0,0 @@
-class PostQuesAddUpvoteFollow < ActiveRecord::Migration
-  def change
-    add_column :post_ques, :upvote, :boolean,:default => false 
-    add_column :post_ques, :follow, :boolean,:default => false 
-
-  end
-end
