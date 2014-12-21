0338648128d76653c084d98c8a1b2fb00124d64f
mecyborg <nikunjboss@gmail.com>
2014-12-21 20:45:42 +0530

    fix-upvote-3

--- db/migrate/20141221150543_change_to_int.rb
+++ db/migrate/20141221150543_change_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeToInt < ActiveRecord::Migration
   def change
-  	change_column :ans_upvotes, :ans_id, :integer
+  	change_column :ans_upvotes, :ans_id, 'integer USING CAST(ans_id AS integer)'
   end
 end
