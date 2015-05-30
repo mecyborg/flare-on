24f26924568bf982769138560615d8a2cb4c9d3d
mecyborg <nikunjboss@gmail.com>
2015-05-30 20:16:06 +0530

    fixed profile error

--- db/migrate/20150526151148_remove.rb
+++
@@ -1,5 +0,0 @@
-class Remove < ActiveRecord::Migration
-  def change
-  	remove_column :users, :profile_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528185538) do
+ActiveRecord::Schema.define(version: 20150530143528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,13 +78,17 @@ ActiveRecord::Schema.define(version: 20150528185538) do
   end
 
   create_table "post_quests", force: :cascade do |t|
-    t.string   "ques_text",  limit: 255
-    t.string   "t_id",       limit: 255
-    t.string   "first",      limit: 255
-    t.string   "upvote",     limit: 255
-    t.string   "follow",     limit: 255
+    t.string   "ques_text",           limit: 255
+    t.string   "t_id",                limit: 255
+    t.string   "first",               limit: 255
+    t.string   "upvote",              limit: 255
+    t.string   "follow",              limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "ques_follows", force: :cascade do |t|
